0bf8cd883faee7e5836c22c2a3be75b0d0ada380
aggressiveasparagus <mbingham@tunzy.com>
2013-04-11 00:53:31 +0200

    Update ftp-simple.js

--- dev/chrome/ftp-simple.js
+++ dev/chrome/ftp-simple.js
@@ -1,15 +1,14 @@
 
-
 var eofHandler = function(blob) {
 	var img = document.getElementById('image');
 	img.src = 'data:image/jpeg;base64,' + btoa(blob);
 };
 
 var client = new Ftp('192.168.1.1', 21, console);
-//client.retrieve('/boxes/flight_20130406_010025/picture_20130406_010025.jpg', eofHandler);
+client.retrieve('/boxes/flight_20130406_010025/picture_20130406_010025.jpg', eofHandler);
 
 client.delete('/boxes/flight_20130406_010025/picture_20130406_010025.jpg', function() {
 	client.removeDirectory('/boxes/flight_20130406_010025', function() {
-		//derp
+		//
 	});
 });
